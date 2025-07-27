70715347cdce4019cc28d9f0e86689a76f65aa29
ylee <1@home>
2025-07-27 10:31:46 +0100

    spaced Title page

--- src/scrypt/element-utils.js
+++ src/scrypt/element-utils.js
@@ -1,3 +1,6 @@
+import { SCRYPT_LINES_PER_PAGE, TOP_MARGIN_LINES, BOTTOM_MARGIN_LINES, MAX_CHARS_PER_LINE } from './default-options.js'
+
+
 export function explodeElement(el) {
   const id = el.id;
   const lines = [], meta = [];
@@ -27,25 +30,53 @@ export function explodeElement(el) {
 export function toLinesAndMap(json) {
   const lines   = [];
   const lineMap = [];          // [{id, field}]  -> keep for reverse lookup
+  let page_no = 0
 
-  const blank = () => { lines.push(""); lineMap.push(null); }
-  const newPage = () => { lines.push(""); lineMap.push({"type": "page_break"}); }
+  const blank = (n= 1) => {
+    lines.push(...Array(n).fill(""));
+    lineMap.push(...Array(n).fill(null));
+  }
+  const newPage = (page_number) => {
+    blank(BOTTOM_MARGIN_LINES);
+    lines.push("")
+    lineMap.push({ "type": "page_break", "page": page_number + '.' });
+    blank(TOP_MARGIN_LINES);
+  }
   const capitalizeFirst = (str) => { if (!str) return str; return str.charAt(0).toUpperCase() + str.slice(1); }
 
-  // Add script meta
-  blank();
-  ['title', 'byline', 'source', 'copyright', 'contact', 'date'].forEach(key => {
-    if (key in json["titlePage"]) {
-      json["titlePage"][key].split(/\r?\n/).forEach((t,i)=>{
+  // Title Page
+  const totalTitlePageLines = SCRYPT_LINES_PER_PAGE + TOP_MARGIN_LINES;
+  const pushLine = (key) => {
+      lines.push(json["titlePage"][key]);
+      lineMap.push({ id:`tp_${key}`, type:key, field:'text', idx:0, label: `${capitalizeFirst(key)}:` });
+  }
+
+  for (let line = 0; line < totalTitlePageLines; line++) {
+    if (line === 7) {
+     pushLine("title");
+    } else if (line === 9) {
+     pushLine("byline");
+    } else if (line === 12) {
+     pushLine("source");
+    } else if (line === 17) {
+     pushLine("copyright");
+    } else if (line === 53) {
+      const key = "contact";
+      const splitValue = json["titlePage"][key].split(/\r?\n/).slice(0,4);
+      splitValue.forEach((t,i)=>{
         lines.push(t);
         const data = { id:`tp_${key}`, type:key, field:'text', idx:i }
         if (i===0) data['label'] = `${capitalizeFirst(key)}:`;
         lineMap.push(data);
       });
+      line += splitValue.length -1;
+    } else if (line === 59) {
+     pushLine("date");
+    } else {
       blank();
     }
-  });
-  newPage();
+  }
+  newPage(++page_no);
 
   json.data.scenes.forEach((sc, sceneIdx) => {
     sc.elements.forEach((el, elIdx) => {
